2d89ceba1384f432311e9eabe9e8bc650abf9e61
Ghaidaamoh <ghaidaa97mohammad@gmail.com>
2021-04-29 17:48:25 +0300

    new change in js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 window.value = 0;
-let learn = prompt('do you think that Im learning programming language right now  ( yes , no , n , y )');
+function learn(){
+    let learn = prompt('do you think that Im learning programming language right now  ( yes , no , n , y )');
 while (learn !== 'yes' && learn !== 'no' && learn !== 'y' && learn !== 'n') {
     learn = prompt('do you think that Im learning programming language right now  ( yes , no , n , y )');
 }
-
 if (learn == 'yes' || learn == 'y') {
     console.log('that is correct');
     alert('that is correct');
@@ -14,8 +14,9 @@ else if (learn == 'no' || learn == 'n') {
     console.log('that is wrong');
     alert('that is wrong');
 }
-
-let college = prompt('do you think I was studied in engineering college  ( yes , no , n , y )');
+}
+function college(){
+    let college = prompt('do you think I was studied in engineering college  ( yes , no , n , y )').toLowerCase();
 while (college !== 'yes' && college !== 'no' && college !== 'y' && college !== 'n') {
     college = prompt('do you think I was studied in engineering college   ( yes , no , n , y )');
 }
@@ -28,7 +29,9 @@ else if (college == 'yes' || college == 'y') {
     console.log('that is wrong');
     alert('that is wrong');
 }
-let status = prompt('do you think I\'m single  ( yes , no , n , y )');
+}
+function status(){
+let status = prompt('do you think I\'m single  ( yes , no , n , y )').toLowerCase();
 while (status !== 'yes' && status !== 'no' && status !== 'y' && status !== 'n') {
     status = prompt('do you think I\'m single  ( yes , no , n , y )');
 }
@@ -41,21 +44,26 @@ else if (status == 'no' || status == 'n') {
     console.log('that is wrong');
     alert('that is wrong');
 }
-let live = prompt('do you think  I\'m living in jordan  ( yes , no , n , y )');
-while (live !== 'yes' && live !== 'no' && live !== 'y' && live !== 'n') {
-    live = prompt('do you think  I\'m living in jordan  ( yes , no , n , y )');
-}
-if (live == 'yes' || live == 'y') {
-    console.log('that is correct');
-    alert('that is correct');
-    value++;
 
 }
-else if (live == 'no' || live == 'n') {
-    console.log('that is wrong');
-    alert('that is wrong');
+function live (){
+    let live = prompt('do you think  I\'m living in jordan  ( yes , no , n , y )').toLowerCase();
+    while (live !== 'yes' && live !== 'no' && live !== 'y' && live !== 'n') {
+        live = prompt('do you think  I\'m living in jordan  ( yes , no , n , y )');
+    }
+    if (live == 'yes' || live == 'y') {
+        console.log('that is correct');
+        alert('that is correct');
+        value++;
+    
+    }
+    else if (live == 'no' || live == 'n') {
+        console.log('that is wrong');
+        alert('that is wrong');
+    }
 }
-let coffee = prompt('do you think I drink  coffee  ( yes , no , n , y )');
+function coffee(){
+    let coffee = prompt('do you think I drink  coffee  ( yes , no , n , y )').toLowerCase();
 while (coffee !== 'yes' && coffee !== 'no' && coffee !== 'y' && coffee !== 'n') {
     coffee = prompt('do you think I drink  coffee  ( yes , no , n , y )');
 }
@@ -69,50 +77,61 @@ else if (coffee == 'yes' || coffee == 'y') {
     console.log('that is wrong');
     alert('that is wrong');
 }
-
-for (let i = 1; i <= 4; i++) {
-    let userTried = prompt('guess what is the number?');
-    console.log(userTried);
-    let num1 = 100;
-    let num2 = 101;
-    let num3 = 102;
-    let num4 = 103;
-    if (userTried >= 150) {
-        alert('too high');
-    }
-    else if (userTried <= 50) {
-        alert('too low');
-    }
-    if (userTried == 100 || userTried == 101 || userTried == 102 || userTried == 103) {
-        value++;
-    }
 }
-
-for (let x = 100; x <= 103; x++) {
-
-    alert("the correct answer is " + x)
-}
-
-
-for (let i = 1; i <= 6; i++) {
-    let userAnswer = prompt('what is my fav food?');
-    let food = ['kabab', 'sushi', 'pizza', 'burger'];
-    let flag = false;
-    for (let v = 0; v < food.length; v++) {
-        if (userAnswer === food[v]) {
-            console.log('yes that\'s one of them');
-            value++;
-            flag = true;
-
+function guess(){
+    for (let i = 1; i <= 4; i++) {
+        let userTried = prompt('guess what is the number?');
+        console.log(userTried);
+        let myFavnum = 100;
+        if (userTried >= myFavnum) {
+            alert('correct');
             break;
-
         }
-
+        if (userTried >= 150) {
+            alert('too high');
+        }
+        else if (userTried <= 50) {
+            alert('too low');
+        }
+        if (userTried == 100 || userTried == 101 || userTried == 102 || userTried == 103) {
+            value++;
+        }
     }
-    if (flag === false) {
-        console.log('sorry it is not one of them');
-
+    
+    for (let x = 100; x <= 103; x++) {
+    
+        alert("the correct answer is " + x)
     }
+    
 }
-alert(" you answered from 7 questions " + value + " welldone ");
+function favFood(){
+    for (let i = 1; i <= 6; i++) {
+        let userAnswer = prompt('what is my fav food?').toLowerCase();
+        let food = ['kabab', 'sushi', 'pizza', 'burger'];
+        let flag = false;
+        for (let v = 0; v < food.length; v++) {
+            if (userAnswer === food[v]) {
+                console.log('yes that\'s one of them');
+                value++;
+                flag = true;
+    
+                break;
+    
+            }
+    
+        }
+        if (flag === false) {
+            console.log('sorry it is not one of them');
+    
+        }
+    }
+    alert(" you answered from 7 questions " + value + " welldone ");
 
+}
+learn();
+college();
+status();
+live();
+coffee();
+guess();
+favFood();
